d572dd89a128f0425043d085c0b53ee433b0ad65
pyne <pie31267@gmail.com>
2021-10-12 23:29:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const socket = require('socket.io');
+// const myip = require('myip');
+// const path = require('path');
+
+const app = express();
+const port = 4000;
+app.use(express.json())
+
+app.get('/', async(req,res)=>{
+    await res.json({"status" :res.statusCode, "message":"OK"});
+})
+app.listen(port, () => console.log(`Listening on ${port}`))
