55219ada0b3813d330b3c239a457ef6012c6cd74
pyne <pie31267@gmail.com>
2021-11-08 00:35:44 +0700

    go to ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
-const express = require('express');
-const { Server } = require('socket.io');
-const { createServer } = require('http');
+import { Server, Socket } from "socket.io";
+import { createServer } from "http"
+import express from "express"
+import path from "path";
 const { Player } = require('./model/client_model.js')
-const path = require('path')
 const app = express();
 const port = 8080;
-
 app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-})
 app.use(express.static(path.join(__dirname, 'controller')))
 
 
@@ -22,27 +13,23 @@ app.get('/', async (req, res) => {
     await res.sendFile(path.join(__dirname, '/view/test.html'))
 })
 
-// app.get('/', (req, res) => {
-//     res.send(Object.keys(io.sockets.clients().connected))
-// })
 const httpServer = createServer(app);
-const io = new Server(httpServer);
-let playerList = [];
-let copyTurn = false;
-let characters = [];
-let numberIn = 0;
-let nowTurn = 0;
-let count = 0;
-let level = 0;
-const MAX_PLAYER = 2;
+const io = new Server(httpServer, { cors: { origin: '*' } });
+let playerList: any = [];
+let characters: string[] = [];
+let copyTurn: boolean = false;
+let numberIn: number = 0;
+let nowTurn: number = 0;
+let count: number = 0;
+let level: number = 0;
+const MAX_PLAYER: number = 2;
 
-const findSocketId = (val, array = playerList) => {
+const findSocketId = (val: string, array = playerList) => {
     for (let i = 0; i < array.length; i++) {
         if (array[i].socketId === val) {
             return i
         }
     }
-    return null;
 }
 
 const randomTurn = () => {
@@ -76,7 +63,7 @@ const resetCollected = () => {
     console.log(characters, "is our stored characters");
 }
 //for checking character and increasing score of that player.
-const checkCharacter = (character) => {
+const checkCharacter = (character: string) => {
     console.log(characters[numberIn], numberIn)
     if (characters[numberIn] === character) {
         if (playerList[0].isTurn) {
@@ -102,19 +89,23 @@ const checkCharacter = (character) => {
     }
 }
 
-const trash = () => {
-    if (playerList[0].isTurn) {
-        playerList[0].score -= 50;
-    } else if (playerList[1].isTurn) {
-        playerList[1].score -= 50;
+const trash = (socketId: string) => {
+    if (playerList.length >= 2) {
+        if (playerList[0].isTurn && playerList[1].sockeId !== socketId) {
+            playerList[0].score -= 50;
+        } else if (playerList[1].isTurn && playerList[0].sockeId !== socketId) {
+            playerList[1].score -= 50;
+        }
+        numberIn--;
+        console.warn('Trash~!')
+        console.log(`player 0 score: ${playerList[0].score}`)
+        console.log(`player 1 score: ${playerList[1].score}`)
+    }else{
+        console.warn("Player <= 2")
     }
-    numberIn--;
-    console.log('Trash~!')
-    console.log(`player 0 score: ${playerList[0].score}`)
-    console.log(`player 1 score: ${playerList[1].score}`)
 }
 
-io.on("connection", (socket) => {
+io.on("connection", (socket: Socket) => {
     const player = new Player(socket.id);
 
     if (playerList.length < MAX_PLAYER) {
@@ -187,7 +178,7 @@ io.on("connection", (socket) => {
             playerList[1].score = 0;
             console.log("    player 1's score:", playerList[1].score)
         }
-        console.log("    level:",level)
+        console.log("    level:", level)
         console.log("    copyTurn:", copyTurn)
         console.log("    numberIn:", numberIn)
         console.log("    turn", nowTurn)
@@ -196,8 +187,8 @@ io.on("connection", (socket) => {
     })
 
     socket.on("enterUsername", (obj) => {
-        playerList[findSocketId(socket.id)].username = obj.username
-        console.log(`Set username ${playerList[findSocketId(socket.id)].username} as "${obj.username}"`)
+        playerList[findSocketId(socket.id)!].username = obj.username
+        console.log(`Set username ${playerList[findSocketId(socket.id)!].username} as "${obj.username}"`)
     })
 
     socket.on("checkPlayer", () => {
@@ -206,36 +197,36 @@ io.on("connection", (socket) => {
         socket.emit("playersInfo", { players: playerList })
     })
     socket.emit("playerInfo", { player: playerList })
-    socket.on('trash', () => { trash() })
+    socket.on('trash', () => { trash(socket.id) })
     socket.on('setDress', (toon, scraf, glasses) => {
-        playerList[findSocketId(socket.id)].setDress(toon, scraf, glasses)
+        playerList[findSocketId(socket.id)!].setDress(toon, scraf, glasses)
     })
     socket.on("getOppData", () => {
-        if(playerList[findSocketId(socket.id)] === playerList[0]){
-            socket.emit("oppData",{oppPlayer: playerList[1]})
-        }else if(playerList[findSocketId(socket.id)] === playerList[1]){
-            socket.emit("oppData",{oppPlayer: playerList[0]})
+        if (playerList[findSocketId(socket.id)!] === playerList[0]) {
+            socket.emit("oppData", { oppPlayer: playerList[1] })
+        } else if (playerList[findSocketId(socket.id)!] === playerList[1]) {
+            socket.emit("oppData", { oppPlayer: playerList[0] })
         }
         // socket.emit("oppData", { oppPlayer: playerList[findSocketId(socket.id) - 1] })
     })
     let x = 0
-    socket.on('setLevel', (obj)=>{
-        if(x===0){
+    socket.on('setLevel', (obj) => {
+        if (x === 0) {
             level = obj.level
-            socket.broadcast.emit('getLevel',{level: level})
+            socket.broadcast.emit('getLevel', { level: level })
         }
         x++
     })
-    socket.on("getNumPlayers",()=>{
-        socket.emit('something',{something: playerList.length})
+    socket.on("getNumPlayers", () => {
+        socket.emit('something', { something: playerList.length })
     })
-    socket.on("getWinner",()=>{
-        if(playerList[0].score > playerList[1].score){
-            socket.emit('something', {something: playerList[0]})
-        }        else if(playerList[0].score < playerList[1].score){
-            socket.emit('something', {something: playerList[1]})
-        } else if(playerList[0].score === playerList[1].score){
-            socket.emit('something', {something: "tied"})
+    socket.on("getWinner", () => {
+        if (playerList[0].score > playerList[1].score) {
+            socket.emit('something', { something: playerList[0] })
+        } else if (playerList[0].score < playerList[1].score) {
+            socket.emit('something', { something: playerList[1] })
+        } else if (playerList[0].score === playerList[1].score) {
+            socket.emit('something', { something: "tied" })
         }
     })
 })
